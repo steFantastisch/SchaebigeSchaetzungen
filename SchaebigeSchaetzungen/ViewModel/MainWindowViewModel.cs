3ff3cdb069acfb975a89afe99b72e38298139efd
simonstenzel <84180936+simonstenzel@users.noreply.github.com>
2022-11-03 11:40:00 +0100

    SIM created

---
+++ SchaebigeSchaetzungen/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SchaebigeSchaetzungen
+{
+	public class MainWindowViewModel
+	{
+		public MainWindowViewModel()
+		{ Spielername=""; }
+		public String Spielername { get; set; }
+
+	}
+}
