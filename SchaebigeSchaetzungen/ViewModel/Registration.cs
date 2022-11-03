e0cc17afaae8f00902f917ed23ad499bf248595d
steFantastisch <84957824+steFantastisch@users.noreply.github.com>
2022-11-03 10:33:01 +0100

    Revert "Bindings zwischen View und ViewModel; events hinzugefügt"
    This reverts commit 8adeec073e9c522ff05576eaf671c29db6502988.

--- SchaebigeSchaetzungen/ViewModel/Registration.cs
+++ SchaebigeSchaetzungen/ViewModel/Registration.cs
@@ -3,57 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace SchaebigeSchaetzungen.ViewModel
 {
-    public class Registration : ViewModelBase
+    internal class Registration : ViewModelBase
     {
-        private string _username;
-
-        public string Username
-        {
-            get
-            {
-                return _username;
-            }
-            set
-            {
-                _username = value;
-                OnPropertyChanged(nameof(Username));
-            }
-        }
-        private string _email;
-        public string Email
-        {
-            get
-            {
-                return _email;
-            }
-            set
-            {
-                _email = value;
-                OnPropertyChanged(nameof(Email));
-            }
-        }
-
-        private string _password;
-        public string Password
-        {
-            get
-            {
-                return _password;
-            }
-            set
-            {
-                _password = value;
-                OnPropertyChanged(nameof(Password));
-            }
-        }
-
-
-        public ICommand BackCommand { get;}
-        public ICommand CreateCommand { get; }
-
     }
 }
