ed2926f858a295cc8fe4c343efc5d54960ece236
simonstenzel <84180936+simonstenzel@users.noreply.github.com>
2022-11-04 14:34:55 +0100

    Buttons klickbar gemacht ohne funktion

---
+++ SchaebigeSchaetzungen/Commands/ToRegistrationView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchaebigeSchaetzungen.Commands
+{
+    internal class ToRegistrationView : CommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+
+        }
+    }
+}
+}
