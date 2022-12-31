51c9da6800c64d86dcde9fe65552c70c6d5098c9
simonstenzel <84180936+simonstenzel@users.noreply.github.com>
2022-12-31 15:59:44 +0100

    new Design Multi und SingleplayerView

---
+++ SchaebigeSchaetzungen/View/MultiplayerGameView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchaebigeSchaetzungen.View
+{
+    /// <summary>
+    /// Interaction logic for MultiplayerGameView.xaml
+    /// </summary>
+    public partial class MultiplayerGameView : UserControl
+    {
+        public MultiplayerGameView()
+        {
+            InitializeComponent();
+            Display("https://www.youtube.com/watch?v=iSfPNVf0_JM");
+        }
+        private Regex YouTubeURLIDRegex = new Regex(@"[?&]v=(?<v>[^&]+)");
+        public void Display(string url)
+        {
+            Match m = YouTubeURLIDRegex.Match(url);
+            String id = m.Groups["v"].Value;
+            string url1 = "http://www.youtube.com/embed/" + id;
+            string page =
+                 "<html>"
+                +"<head><meta http-equiv='X-UA-Compatible' content='IE=11' />"
+                + "<body>" + "\r\n"
+                + "<iframe src=\"" + url1 +  "\" width=\"100%\" height=\"100%\" frameborder=\"0\" allowfullscreen></iframe>"
+                + "</body></html>";
+            webBrowser1.NavigateToString(page);
+
+        }
+    }
+}
