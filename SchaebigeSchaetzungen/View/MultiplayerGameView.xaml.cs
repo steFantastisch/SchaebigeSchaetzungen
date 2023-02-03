90803fb48f7bbe965bb51d7ab40177e4caa22a42
simonstenzel <84180936+simonstenzel@users.noreply.github.com>
2023-02-03 13:38:20 +0100

    Hints und Labels in MP

--- SchaebigeSchaetzungen/View/MultiplayerGameView.xaml.cs
+++ SchaebigeSchaetzungen/View/MultiplayerGameView.xaml.cs
@@ -57,44 +57,57 @@ namespace SchaebigeSchaetzungen.View
 
         private void P1Submit_Click(object sender, RoutedEventArgs e)
         {
-            if (P1Submit.Content.ToString() == "Submit")
-            {
-                if (!Int32.TryParse(TextBox1.Text, out PlayerOneguess))
-                {
-                    //TODO
-                    //Fehlermessage wegen falscheingabe
-                    return;
-                }
 
+            if (!Int32.TryParse(TextBox1.Text, out PlayerOneguess))
+            {
+                //TODO
+                //Fehlermessage wegen falscheingabe
+                return;
+            }
+            if (P2Submit.Visibility==Visibility.Collapsed) //Zweiter spieler hat schon getippt
+            {
+                P1Submit.Visibility= Visibility.Collapsed;
+                TextBox1.Visibility= Visibility.Collapsed;
+                P1.Visibility= Visibility.Collapsed;
                 //TODO change to 3
-                if (round > 1) // Maximum 5 Runden
+                if (round > 1) // Spiel vorbei?
                 {
-                    SubmitBtn.Content="Result";
-                    SubmitBtn.Visibility= Visibility.Collapsed;
                     ResultBtn.Visibility= Visibility.Visible;
                 }
                 else
                 {
-                    SubmitBtn.Content="Next Round";
+                    NextRound.Visibility= Visibility.Visible;
                 }
-                SubmitBtn.Content="Next Round";
-                TextBox1.Visibility= Visibility.Collapsed;
-                GuessLabel.Visibility= Visibility.Collapsed;
-                HintCheckBox.Visibility= Visibility.Collapsed;
-                HintLikes.Visibility = Visibility.Collapsed;
-                HintComments.Visibility = Visibility.Collapsed;
-
-                CalcPoints Rechner = new CalcPoints();
-                ViewLabel.Content= Rechner.SingleplayerPts(guess, viewCount);
-
-                HintLabel.Content= " Du lagst " +Math.Abs(viewCount - guess)+ " von der richtigen Lösung weg!";
-
-                ViewLabel.Visibility = Visibility.Visible;
-                LanguageLabel.Content="Language: "+language;
-                LanguageLabel.Visibility = Visibility.Visible;
-                return;
-
             }
+            else //Zweiter Spieler noch nicht getippt
+
+            {
+                P1Submit.Visibility= Visibility.Collapsed;
+                TextBox1.Text="******************";            
+            }
+
+            else
+            {
+                SubmitBtn.Content="Next Round";
+            }
+            SubmitBtn.Content="Next Round";
+            TextBox1.Visibility= Visibility.Collapsed;
+            GuessLabel.Visibility= Visibility.Collapsed;
+            HintCheckBox.Visibility= Visibility.Collapsed;
+            HintLikes.Visibility = Visibility.Collapsed;
+            HintComments.Visibility = Visibility.Collapsed;
+
+            CalcPoints Rechner = new CalcPoints();
+            ViewLabel.Content= Rechner.SingleplayerPts(guess, viewCount);
+
+            HintLabel.Content= " Du lagst " +Math.Abs(viewCount - guess)+ " von der richtigen Lösung weg!";
+
+            ViewLabel.Visibility = Visibility.Visible;
+            LanguageLabel.Content="Language: "+language;
+            LanguageLabel.Visibility = Visibility.Visible;
+            return;
+
+
 
             else if (SubmitBtn.Content.ToString() == "Next Round")
             {
@@ -123,6 +136,24 @@ namespace SchaebigeSchaetzungen.View
         private void P2Submit_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void NextRound_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HintCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            HintLikes.Content = "Likes: " + likeCount.ToString();
+            HintLikes.Visibility = Visibility.Visible;
+            HintComments.Content = "Comments: "+ commentCount.ToString();
+            HintComments.Visibility = Visibility.Visible;
+        }
+        private void HintCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            HintLikes.Visibility = Visibility.Collapsed;
+            HintComments.Visibility = Visibility.Collapsed;
         }
     }
 }
